e96fbc338732fd664a110a4f594b659675c66ee7
TomasBubenik <91113218+TomasBubenik@users.noreply.github.com>
2021-09-21 09:29:23 +0200

    Add files via upload

--- Priemer.py
+++ Priemer.py
@@ -6,7 +6,7 @@ while z:
     a = int(input("Zadaj číslo:"))
     b = str(input("Chceš pokračovať?y/n:"))
     if (b == "n"):
-        print (x/y) 
+        print ("Priemer tvojich čisiel je:",x/y)
     x=y+a
     y=x+a
 
